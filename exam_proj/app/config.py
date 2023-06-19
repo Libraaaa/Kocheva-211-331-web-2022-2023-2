f0d77f030b8e59ade3c91bf417d806925515c5be
Libraaaa <elizavetakocheva0310@mail.ru>
2023-06-19 15:34:28 +0300

    exam_proj full

--- exam_proj/app/config.py
+++ exam_proj/app/config.py
@@ -1,8 +1,8 @@
 import os
 
-SECRET_KEY = '9695cce7d2daa4d9c7f952d1cca92b6c9183e5907262017d987e60de07801749'
+SECRET_KEY = ''
 
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_1861_exam_proj:qwertyqq@std-mysql.ist.mospolytech.ru/std_1861_exam_proj'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_1861_exam_proj:@std-mysql.ist.mospolytech.ru/std_1861_exam_proj'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
 
